9ecb1e5e9a690963e95f424491bf699a4b32bae1
sammy26 <36407773+sammy26@users.noreply.github.com>
2018-02-20 17:35:19 +0530

    Add files via upload

---
+++ sam.7.py
@@ -0,0 +1,3 @@
+sam=input("Enter a string")
+n=input("No of times to repeat:")
+print(sam * int(n))
---
+++ sam.8.py
@@ -0,0 +1,6 @@
+sam=int(input("Enter a number: "))
+sum1 = 0
+while(sam > 0):
+    sum1=sum1+sam
+    sam=sam-1
+print("The sum of first n natural numbers is",sum1)
